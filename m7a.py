5d47a892bffb0d7e3a93f8cc47baf8b2db9b07f6
Matthew Mason <matthewmason@ufl.edu>
2025-02-25 16:33:01 -0500

    Add files via upload

---
+++ m7a.py
@@ -0,0 +1,43 @@
+#Decimal to binary function
+def bin(decimal):
+    Binary=[]
+    if decimal==0:
+        return 0
+    while decimal>=1:
+        Binary.append(decimal%2)
+        decimal = decimal//2
+    return Binary[::-1]
+
+#Capitalze function
+def capitalize(str):
+    NewStr=str.split(" ")
+    ReturnList=[]
+    ReturnString=""
+    for word in NewStr:
+        if word[0].lower() in ["o","u","s","n","d"]:
+            ReturnList.append(word[::].lower())
+        else:
+            ReturnList.append(word[0].capitalize()+word[1:].lower())
+    for word in ReturnList:
+        ReturnString+=word
+        ReturnString+=" "
+    return ReturnString[:len(ReturnString)-1]
+
+#List partition function
+numbers = [1,2,3,4,5,6,7,8,9,10]
+def partition(list, length):
+    NewList=[]
+    Bound=length
+    BoundUpper=Bound
+    Iteration=0
+    for i in range(0,len(list), length):
+        NewList.append("")
+        NewList[Iteration]=[]
+        if BoundUpper>len(list):
+            BoundUpper=len(list)
+        for j in range(Bound-length,BoundUpper):
+            NewList[Iteration].append(list[j])
+        Bound+=length
+        BoundUpper+=length
+        Iteration+=1
+    return NewList
