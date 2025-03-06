79d2b2d25976c2e10c208ed4ff7f60801469412a
Matthew Mason <matthewmason@ufl.edu>
2025-03-06 20:35:14 +0000

    Update Lab6.py

--- Lab6.py
+++ Lab6.py
@@ -1,80 +1,72 @@
 #Connect 4 Lab
-#Initialize board
-def initialize_board(num_rows, num_cols):
-    Board = []
-    #Makes the rows
-    for i in range(num_rows):
-        Board.append([])
-        #Makes the columns
-        for j in range(num_cols):
-            Board[i].append("-")
-    return Board
-
-#Print board
-def print_board(board):
-    for index, val in enumerate(board):
-        for j in board[index]:
-            print(j,end=" ")
-        print()
-
-#Insert chip
-def insert_chip(board, col, chip_type):
-    for i, j in reversed(list(enumerate(board))):
-        for val in board[i][col]:
-            if val == "-":
-                return i
-
-#Check winner
-def check_if_winner(board, col, row, chip_type):
-    GameBoard[row][col] = chip_type
-    #Check across row
-    InRow=0
-    for val in GameBoard[row]:
-        if val != chip_type:
-            InRow = -1
-        InRow+=1
-        if InRow == 4:
-            Checks[0] = True
-            break
-    #Check across column
-    InRow=0
-    for index, i in enumerate(GameBoard):
-        if GameBoard[index][col] != chip_type:
-            InRow = -1
-        InRow+=1
-        if InRow == 4:
-            Checks[1] = True
-            break
-    #Check for draw
-    for index, i in enumerate(GameBoard):
-        Check = False
-        for val in GameBoard[index]:
-            if val == "-":
-                Checks[2] = False
-                Check = True
-                break
-        if Check:
-            break
-        Checks[2] = True
-    return (Checks[0] or Checks[1]) or Checks[2]
-
 def main():
-    GameWon = False
-    Turn = 1
-    Checks = [False, False, False]
-    try:
-        Height = int(input("What would you like the height of the board to be? "))
-    except:
-        Height = 0
-        GameWon = True
-    try:
-        Length = int(input("What would you like the length of the board to be? "))
-    except:
-        Length = 0
-        GameWon = True
+    #Initialize board
+    def initialize_board(num_rows, num_cols):
+        Board = []
+        #Makes the rows
+        for i in range(num_rows):
+            Board.append([])
+            #Makes the columns
+            for j in range(num_cols):
+                Board[i].append("-")
+        return Board
+
+    #Print board
+    def print_board(board):
+        for index, val in enumerate(board):
+            for j in board[index]:
+                print(j,end=" ")
+            print()
+
+    #Insert chip
+    def insert_chip(board, col, chip_type):
+        for i, j in reversed(list(enumerate(board))):
+            for val in board[i][col]:
+                if val == "-":
+                    return i
+
+    #Check winner
+    def check_if_winner(board, col, row, chip_type):
+        GameBoard[row][col] = chip_type
+        #Check across row
+        InRow=0
+        for val in GameBoard[row]:
+            if val != chip_type:
+                InRow = -1
+            InRow+=1
+            if InRow == 4:
+                Checks[0] = True
+                break
+        #Check across column
+        InRow=0
+        for index, i in enumerate(GameBoard):
+            if GameBoard[index][col] != chip_type:
+                InRow = -1
+            InRow+=1
+            if InRow == 4:
+                Checks[1] = True
+                break
+        #Check for draw
+        for index, i in enumerate(GameBoard):
+            Check = False
+            for val in GameBoard[index]:
+                if val == "-":
+                    Checks[2] = False
+                    Check = True
+                    break
+            if Check:
+                break
+            Checks[2] = True
+        return (Checks[0] or Checks[1]) or Checks[2]
+
+    Height = int(input("What would you like the height of the board to be? "))
+    Length = int(input("What would you like the length of the board to be? "))
     GameBoard = initialize_board(Height, Length)
     print_board(GameBoard)
     print()
+    Turn = 1
+    GameWon = False
+    Checks = [False, False, False]
     print("Player 1: x\nPlayer 2: o\n")
 
     #Game loop
@@ -101,8 +93,4 @@ def main():
             print("Player 1 won the game!")
 
 if __name__ == "__main__":
-    Turn = 1
-    Checks = [False, False, False]
-    GameWon = False
-    GameBoard = []
     main()
