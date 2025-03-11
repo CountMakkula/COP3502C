e5ad1735feabffe4115670131216e44d65148fd6
Matthew Mason <matthewmason@ufl.edu>
2025-03-11 16:23:27 -0400

    Update m8.py

--- m8.py
+++ m8.py
@@ -24,28 +24,21 @@ def mystery1(n):
 
 #Recursion/Loop Conversion function 2
 def mystery2(number):
-    def inner(number, digit, total):
-        if number // 10 <= 0:
+    def inner(number, total):
+        if number <= 0:
             return total
-        return inner(number // 10, number % 10, total + (number % 10))
-    return inner(number, number % 10, number % 10)
-
-def mystery3(number):
-    total = 0
-    while number > 0:
-        digit = number % 10
-        total += digit
-        number //= 10
-    return total
+        return inner(number // 10, total + (number % 10))
+    return inner(number, 0)
 
 #Collatz Sequence function
+
 def collatz_sequence(n):
     def inner(n):
         if n % 2 == 0:
             print(f"{n:.0f}", end=" ")
             return collatz_sequence(n / 2)
         elif n == 1:
-            print(1, end = " \n")
+            print(1, end=" \n")
             return 1
         else:
             print(f"{n:.0f}", end=" ")
