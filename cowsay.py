c4e1032a0923179c20447c4d5b994845e3b29626
Matthew Mason <matthewmason@ufl.edu>
2025-04-02 23:30:25 +0000

    Update cowsay.py

--- cowsay.py
+++ cowsay.py
@@ -2,7 +2,7 @@
 #Import libraries
 import sys
 from heifer_generator import get_cows
-from cow import Cow
+from dragon import Dragon
 
 
 #List cows function
@@ -17,7 +17,7 @@ def list_cows(cows):
 def find_cow(name, cows):
     for i in cows:
         if i.name == name:
-            return i.image
+            return i.image, i
     return None
 
 #Main function
@@ -28,14 +28,19 @@ def main():
     # -n command
     elif sys.argv[1] == "-n":
         SelectedCow = find_cow(sys.argv[2], get_cows())
-        if SelectedCow is None:
+        if SelectedCow[0] is None:
             print(f"Could not find {sys.argv[2]} cow!")
         else:
             Message = ""
             for i in sys.argv[3:]:
                 Message += f"{i} "
             print(Message[:-1])
-            print(SelectedCow)
+            print(SelectedCow[0])
+            if isinstance(SelectedCow[1], Dragon):
+                if SelectedCow[1].can_breath_fire():
+                    print("This dragon can breathe fire.")
+                else:
+                    print("This dragon cannot breathe fire.")
     # Message
     else:
         Message = ""
