623b5693d0c557b899813c95044f32ff39fd84c1
Matthew Mason <matthewmason@ufl.edu>
2025-04-08 20:27:05 +0000

    Update m11.py

--- m11.py
+++ m11.py
@@ -5,6 +5,15 @@ import unittest
 class TestCase(unittest.TestCase):
     def test_prime1(self):
         self.assertTrue(is_prime(7))
-        
+        self.assertFalse(is_prime(6))
+        self.assertFalse(is_prime(1))
+        self.assertTrue(is_prime(2))
+
+    def test_prime2(self):
+        self.assertFalse(is_prime(-3))
+
+    def test_prime3(self):
+        self.assertFalse(is_prime(2))
+
 if __name__ == "__main__":
     unittest.main()
