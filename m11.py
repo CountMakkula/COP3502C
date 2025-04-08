369c1a95dc61e61ca0804b08c575d9028f48d07a
Matthew Mason <matthewmason@ufl.edu>
2025-04-08 20:07:39 +0000

    Update m11.py

--- m11.py
+++ m11.py
@@ -1,72 +1,9 @@
-from base_classes import *
-#Secure account class
-class SecureAccount(Account):
-    def __init__(self, password):
-        super().__init__()
-        self.password = password
+#Import libraries
+from functions import *
+import unittest
 
-    def get_balance(self, password):
-        if password != self.password:
-            print("Incorrect password")
-        else:
-            return super().get_balance()
-
-    def deposit(self, amount, password):
-        if password != self.password:
-            print("Incorrect password")
-        else:
-            return super().deposit(amount)
-
-    def withdraw(self, amount, password):
-        if password != self.password:
-            print("Incorrect password")
-        else:
-            return super().withdraw(amount)
-
-#Memory calculator class
-class MemoryCalculator(Calculator):
-    def __init__(self):
-        super().__init__()
-        self.result = 0
-
-    def add(self, x, y):
-        if x == "RESULT":
-            x = self.result
-        if y == "RESULT":
-            y = self.result
-        self.result = super().add(x, y)
-        return self.result
-
-    def sub(self, x, y):
-        if x == "RESULT":
-            x = self.result
-        if y == "RESULT":
-            y = self.result
-        self.result = super().sub(x, y)
-        return self.result
-
-#Fractions class
-class ImprovedFraction(Fraction):
-    def add(self, other):
-        if type(other) == int:
-            N = ImprovedFraction(other, 1)
-            return super().add(N)
-        else:
-            return super().add(other)
-
-    def multiply(self, other):
-        if type(other) == int:
-            N = ImprovedFraction(other, 1)
-            return super().multiply(N)
-        else:
-            return super().multiply(other)
-        return self
-
-    def __add__(self, other):
-        return self.add(other)
-
-    def __mul__(self, other):
-        return self.multiply(other)
-
-    def __str__(self):
-        return f"{self._num}/{self._den}"
+class TestCase1(unittest.TestCase):
+    def TestPrime(self):
+        self.assertTrue(is_prime(7))
+        self.assertFalse(is_prime(6))
+        self.assertFalse(is_prime(1))
